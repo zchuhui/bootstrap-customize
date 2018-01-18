b3d13cbe7a6116714a1273311187da335864f93d
zchuhui <943339564@qq.com>
2018-01-18 18:21:01 +0800

    添加注册

--- demo1/javascripts/login.js
+++ demo1/javascripts/login.js
@@ -1,14 +1,45 @@
 
 require.config({
   paths: {
-    "jquery":"./lib/jquery-3.2.1.min",
-    "jquery.validate":"./lib/jquery.validate"
+    "jquery": "./lib/jquery-3.2.1.min",
+    "jquery.validate": "./lib/jquery.validate",
+    "bootstrapValidator": "./lib/bootstrapValidator"
   }
 });
 
-require(["jquery","jquery.validate"], function(){
+require(["jquery", "bootstrapValidator"], function ($) {
 
-  $("form").validate();
+  $('form').bootstrapValidator({
+    message: 'This value is not valid',
+    // 提示图标
+    feedbackIcons: {
+      //valid: 'glyphicon glyphicon-ok',
+      //invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      password: {
+        message: '密码验证失败',
+        validators: {
+          notEmpty: {
+            message: '密码不能为空'
+          },
+          stringLength: {
+            min: 6,
+            max: 18,
+            message: 'password length error'
+          },
+        }
+      },
+      email: {
+        validators: {
+          notEmpty: {
+            message: '邮箱地址不能为空'
+          }
+        }
+      }
+    }
+  });
 
 });
 
